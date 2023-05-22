0eab1034749cbd87bde195479f63b0cd24d44799
Katharine Harshman <katieharshman1011@gmail.com>
2023-05-21 19:33:51 -0700

    added CodeyOverflow per project instructions

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Body(props) {
+  return (
+    <div>
+      <p>{props.comment}</p>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+
+
+function Card(props) {
+    return (
+        <>
+            <Header profileImg = {props.commentObject.profileImg} username = {props.commentObject.username} />
+            <Body />
+        </>
+    );
+}
+
+export default Card;
